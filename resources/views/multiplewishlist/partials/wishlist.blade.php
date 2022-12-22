57d166105c5ab9ab449c785502ad76b3584b4e30
Jade Geels <jade@justbetter.nl>
2022-12-22 07:41:38 +0100

    Reworked list render to be in reactivelist renderer

--- resources/views/multiplewishlist/partials/wishlist.blade.php
+++ resources/views/multiplewishlist/partials/wishlist.blade.php
@@ -15,58 +15,47 @@
         <p>@{{ data[0].description }}</p>
     </div>
     <listing v-cloak>
-        <div slot-scope="{ loaded, filters, sortOptions, reactiveFilters }" class="hidden">
+        <div slot-scope="{ loaded }">
             <reactive-base :app="config.es_prefix + '_products_' + config.store" :url="config.es_url" v-if="loaded">
                 <reactive-list id="products" component-id="products" data-field="sku.keyword" :default-query="function() { return { query: { terms: { 'sku.keyword': data[1].map(e => e.sku) } } } }">
-                    <div slot="renderItem" slot-scope="{ item }" :set="$root.custom.currentWishlistData[1].filter(e => e.sku == item.sku).forEach(e => e['item'] = item)" :setloaded="$root.custom.wishlistLoaded=true"></div>
+                    <div slot="renderResultStats"></div>
+                    <div slot="renderNoResults">This wishlist is empty.</div>
+                    <div slot="render" slot-scope="{ data, dataItem }" class="flex flex-col">
+                        <div class="self-end p-4" v-if="data.length > 1">
+                            <x-rapidez::button
+                                type="primary"
+                                @click.prevent="$root.$refs['addToCart'].forEach(e => e.click())"
+                            >
+                                Add all items to cart
+                            </x-rapidez::button>
+                        </div>
+                        <div v-for="item in data" class="rounded-md even:bg-gray-200 flex items-center justify-between p-4" :set="dataItem=$root.custom.currentWishlistData[1].find(e => e.sku == item.sku)">
+                            <template v-if="dataItem">
+                                <a :href="item.url" class="flex gap-2 w-full">
+                                    <picture v-if="item.thumbnail">
+                                        <source :srcset="'/storage/resizes/200/catalog/product' + item.thumbnail + '.webp'" type="image/webp">
+                                        <img :src="'/storage/resizes/200/catalog/product' + item.thumbnail" class="object-contain rounded-md h-40" :alt="item.name" loading="lazy" />
+                                    </picture>
+                                    <div class="w-full flex flex-col">
+                                        <span class="font-bold text-20">@{{ item.name }}</span>
+                                        <span>@{{ item.price | price }}</span>
+                                    </div>
+                                </a>
+                                <div class="flex gap-3 pr-10">
+                                    <template v-if="dataItem.type_id == 'simple'">
+                                        @include('rapidez::multiplewishlist.partials.addtocart', ['product' => 'item'])
+                                    </template>
+                                    @if($editable)
+                                        <api-request method="delete" :destination="'wishlists/item/' + dataItem.id" :variables="{ wishlistId: {{ $id }} }" v-slot="{ runQuery }" :callback="() => window.Vue.delete($root.custom.currentWishlistData[1], index)">
+                                            <button class="w-14 h-14 border border-red-600 bg-red-600 text-white hover:bg-white hover:text-red-600 transition rounded-md" @click="runQuery">X</button>
+                                        </api-request>
+                                    @endif
+                                </div>
+                            </template>
+                        </div>
+                    </div>
                 </reactive-list>
             </reactive-base>
         </div>
     </listing>
-    <div class="flex flex-col">
-        <template v-if="$root.custom.wishlistLoaded">
-            <div class="self-end p-4">
-                <x-rapidez::button
-                    type="primary"
-                    @click.prevent="$root.$refs['addToCart'].forEach(e => e.click())"
-                >
-                    Add all items to cart
-                </x-rapidez::button>
-            </div>
-            <div
-                v-for="(item, index) in $root.custom.currentWishlistData[1]"
-                :key="item.id"
-                class="rounded-md even:bg-gray-200 flex items-center justify-between p-4"
-            >
-                <template v-if="item">
-                    <a :href="item.item.url" class="flex gap-2 w-full">
-                        <picture v-if="item.item.thumbnail">
-                            <source :srcset="'/storage/resizes/200/catalog/product' + item.item.thumbnail + '.webp'" type="image/webp">
-                            <img :src="'/storage/resizes/200/catalog/product' + item.item.thumbnail" class="object-contain rounded-md h-40" :alt="item.name" loading="lazy" />
-                        </picture>
-                        <div class="w-full flex flex-col">
-                            <span class="font-bold text-20">@{{ item.item.name }}</span>
-                            <span>@{{ item.item.price | price }}</span>
-                        </div>
-                    </a>
-                    <div class="flex gap-3 pr-10">
-                        <template v-if="item.type_id == 'simple'">
-                            @include('rapidez::multiplewishlist.partials.addtocart', ['product' => 'item.item'])
-                        </template>
-                        @if($editable)
-                            <api-request method="delete" :destination="'wishlists/item/' + item.id" :variables="{ wishlistId: {{ $id }} }" v-slot="{ runQuery }" :callback="() => window.Vue.delete($root.custom.currentWishlistData[1], index)">
-                                <button class="w-14 h-14 border border-red-600 bg-red-600 text-white hover:bg-white hover:text-red-600 transition rounded-md" @click="runQuery">X</button>
-                            </api-request>
-                        @endif
-                    </div>
-                </template>
-            </div>
-        </template>
-        <div v-else-if="$root.custom.currentWishlistData[1].length == 0" class="text-gray-400 text-sm">
-            This wishlist is empty.
-        </div>
-        <div v-else class="flex justify-center text-gray-400 text-sm">
-            Loading...
-        </div>
-    </div>
 </div>
