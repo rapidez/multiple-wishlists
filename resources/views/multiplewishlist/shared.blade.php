0f79f9585b2d6100b2ed9c7ad2404a973e6bd127
Jade Geels <jade@justbetter.nl>
2022-12-22 09:55:23 +0100

    Shared wishlist in different layout

--- resources/views/multiplewishlist/shared.blade.php
+++ resources/views/multiplewishlist/shared.blade.php
@@ -1,11 +1,14 @@
-@extends('rapidez::account.partials.layout')
+@extends('rapidez::layouts.app')
 
-@section('title', __('Wishlist'))
+@section('title', __('Shared wishlist'))
 
 @section('robots', 'NOINDEX,NOFOLLOW')
 
-@section('account-content')
-    <api-request method="get" immediate destination="wishlists/shared/{{ $token }}" v-slot="{ data, runQuery }" checkfail="/" check="data[0].title">
-        @include('rapidez::multiplewishlist.partials.wishlist', ['editable' => false])
-    </api-request>
+@section('content')
+    <div class="container mx-auto" v-cloak>
+        <h1 class="font-bold text-4xl my-3">@yield('title')</h1>
+        <api-request method="get" immediate destination="wishlists/shared/{{ $token }}" v-slot="{ data, runQuery }" checkfail="/" check="data[0].title">
+            @include('rapidez::multiplewishlist.partials.wishlist', ['editable' => false])
+        </api-request>
+    </div>
 @endsection
