5fb684cf666e3ed50d818bc73d1cc9dc423075a3
Roene <roene@justbetter.nl>
2024-02-08 12:08:22 +0100

    Changed arbitrary values to newest tailwind version

--- resources/views/components/table/header.blade.php
+++ resources/views/components/table/header.blade.php
@@ -1,5 +1,5 @@
 <thead {{ $attributes->class(['w-full rounded bg-ct-inactive-100']) }}>
-    <tr class="text-xs text-ct-inactive [&>*]:py-4 [&>*]:font-normal [&>*]:px-5">
+    <tr class="text-xs text-ct-inactive *:py-4 *:font-normal *:px-5">
         {{ $slot }}
     </tr>
 </thead>
