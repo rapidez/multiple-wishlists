bbeb58e08898b2fb2ae9b30da74a9cb9f202369b
Roene <roene@justbetter.nl>
2024-02-12 11:10:51 +0100

    Added variable for wishlist button

--- resources/views/partials/item/button.blade.php
+++ resources/views/partials/item/button.blade.php
@@ -1,5 +1,5 @@
 <button v-if="$root.loggedIn" class="p-3" @click="toggle">
-    <x-heroicon-s-heart v-if="isWishlisted({{ $productId }})" class="w-5 h-5 text-ct-error hover:opacity-80" />
+    <x-heroicon-s-heart v-if="isWishlisted({{ $productId }})" class="w-5 h-5 text-ct-wishlist hover:opacity-80" />
     <x-heroicon-o-heart v-else v-bind:class="{ 'text-ct-primary': isOpen, 'text-ct-border hover:text-ct-primary': !isOpen }" class="w-5 h-5 transition" />
 </button>
 
