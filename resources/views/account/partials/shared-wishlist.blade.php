c7368360ca28e70e7d3e3404142ff6f4dadf3aed
Jade Geels <jade@justbetter.nl>
2024-02-12 13:58:31 +0100

    Move parentheses

--- resources/views/account/partials/shared-wishlist.blade.php
+++ resources/views/account/partials/shared-wishlist.blade.php
@@ -1,3 +1,3 @@
 <a v-if="wishlist.shared" :href="'{{ route('wishlist.shared', '') }}/' + wishlist.sharing_token" class="text-ct-inactive text-sm hover:underline">
-    @lang('(shared)')
+    (@lang('shared'))
 </a>
