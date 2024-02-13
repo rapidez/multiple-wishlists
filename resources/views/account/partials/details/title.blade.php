e1c477957e0f4180a5dcbd0674b8215ac7ff0a24
Roene <roene@justbetter.nl>
2024-02-13 09:15:15 +0100

    Fix styling input - textarea - checkbox

--- resources/views/account/partials/details/title.blade.php
+++ resources/views/account/partials/details/title.blade.php
@@ -21,13 +21,15 @@
             <form class="flex flex-col" v-on:submit.stop="save">
                 <div class="flex items-center">
                     <x-rapidez-ct::title href="/account/wishlists"></x-rapidez-ct::title>
-                    <input type="text" v-model="editing.title">
-                    <x-rapidez-mw::button.accent class="ml-4 flex items-center justify-center !p-0 w-9 h-9">
-                        <x-heroicon-o-check class="w-4 h-4 text-white" />
-                    </x-rapidez-mw::button.accent>
+                    <div class="relative border rounded">
+                        <x-rapidez::input class="w-80 border-0 pr-12" name="Edit title" type="text" v-model="editing.title" />
+                        <x-rapidez-mw::button.accent class="!absolute -translate-y-1/2 top-1/2 right-1  ml-4 flex items-center justify-center !p-0 w-9 h-9">
+                            <x-heroicon-o-pencil-square class="w-4 h-4 text-white" />
+                        </x-rapidez-mw::button.accent>
+                    </div>
                 </div>
-                <textarea v-model="editing.description"></textarea>
-                <input type="checkbox" v-model="editing.shared"> @lang('Share this wishlist')
+                <x-rapidez::textarea class="mt-2.5" label="" name="Description" v-model="editing.description"></x-rapidez::textarea>
+                <x-rapidez-ct::input.checkbox class="mt-2.5" v-model="editing.shared">@lang('Share this wishlist')</x-rapidez::checkbox>
             </form>
         </template>
     @endif
