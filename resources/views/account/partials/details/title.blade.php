2d2087f909ee5180d530cdf13bcd49399dff9771
Roene <roene@justbetter.nl>
2024-02-13 10:38:53 +0100

    Only show button when editing wishlist name

--- resources/views/account/partials/details/title.blade.php
+++ resources/views/account/partials/details/title.blade.php
@@ -1,13 +1,6 @@
 <div class="flex flex-col">
     <template v-if="!editing">
-        <div class="flex items-center">
-            <x-rapidez-ct::title href="/account/wishlists">@{{ wishlist.title }}</x-rapidez-ct::title>
-            @if ($editable)
-                <x-rapidez-mw::button.accent class="ml-4 flex items-center justify-center !p-0 w-9 h-9" v-on:click="toggleEdit">
-                    <x-heroicon-o-pencil-square class="w-4 h-4 text-white" />
-                </x-rapidez-mw::button.accent>
-            @endif
-        </div>
+        <x-rapidez-ct::title href="/account/wishlists">@{{ wishlist.title }}</x-rapidez-ct::title>
         <div v-if="wishlist.shared" class="text-sm text-ct-primary flex flex-col">
             <span>@lang('Sharing link'): <a :href="shareUrl" class="text-ct-inactive underline">@{{ shareUrl }}</a></span>
             <template v-if="isSupported">
