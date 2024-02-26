5c4c496606cad033d14a674bbf5f950e6909cb6f
Jade Geels <jade@justbetter.nl>
2024-02-26 16:04:14 +0100

    Support for Rapidez v2

--- resources/js/stores/useWishlists.js
+++ resources/js/stores/useWishlists.js
@@ -5,6 +5,10 @@ import { token } from 'Vendor/rapidez/core/resources/js/stores/useUser'
 export const wishlistStorage = useLocalStorage('wishlists', [])
 let isRefreshing = false
 let hasFetched = false
+let fetchHeaders = () => ({
+    Authorization: `Bearer ${token.value}`,
+    Store: window.config.store_code,
+})
 
 export const refresh = async function () {
     if (!token.value) {
@@ -17,33 +21,15 @@ export const refresh = async function () {
         return
     }
 
+    isRefreshing = true
     try {
-        isRefreshing = true
-        var response = await axios({
-            method: 'GET',
-            url: window.url('/api/wishlists'),
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-                Store: window.config.store_code,
-            },
-        }).finally(() => {
-            isRefreshing = false
-        })
+        wishlistStorage.value = (await window.rapidezAPI('GET', 'wishlists', {}, { headers: fetchHeaders() })) || []
+        hasFetched = true
     } catch (error) {
         console.error(error)
         Notify(window.config.translations.errors.wrong, 'error')
-
-        return false
     }
-
-    if (response === undefined || !response.data) {
-        return false
-    }
-
-    hasFetched = true
-    wishlistStorage.value = response.data
-
-    return true
+    isRefreshing = false
 }
 
 export const clear = async function () {
@@ -53,7 +39,7 @@ export const clear = async function () {
 
 export const wishlists = computed({
     get() {
-        if (!hasFetched && Object.keys(wishlistStorage.value).length === 0) {
+        if (!hasFetched && wishlistStorage.value.length === 0) {
             refresh()
         }
 
@@ -70,23 +56,11 @@ export default () => wishlists
 
 export const create = async function (title) {
     try {
-        let response = await axios({
-            method: 'POST',
-            url: window.url('/api/wishlists'),
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-                Store: window.config.store_code,
-            },
-            data: {
-                title: title
-            },
-        })
+        let response = await window.rapidezAPI('POST', 'wishlists', { title: title }, { headers: fetchHeaders() })
 
-        let data = {
-            items: [],
-            ...response.data,
-        }
+        let data = { items: [], ...response }
         wishlists.value.push(data)
+
         return data
     } catch (error) {
         console.error(error)
@@ -98,14 +72,7 @@ export const create = async function (title) {
 
 export const remove = async function (id) {
     try {
-        await axios({
-            method: 'DELETE',
-            url: window.url('/api/wishlists/' + id),
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-                Store: window.config.store_code,
-            },
-        })
+        await window.rapidezAPI('DELETE', 'wishlists/' + id, {}, { headers: fetchHeaders() })
 
         let index = wishlists.value.findIndex(e => e.id == id)
         wishlists.value.splice(index, 1)
@@ -122,24 +89,17 @@ export const remove = async function (id) {
 export const update = async function (id, data) {
     try {
         if ('title' in data || 'description' in data || 'shared' in data) {
-            let response = await axios({
-                method: 'PATCH',
-                url: window.url('/api/wishlists/' + id),
-                headers: {
-                    Authorization: `Bearer ${token.value}`,
-                    Store: window.config.store_code,
-                },
-                data: {
-                    title: data.title,
-                    description: data.description,
-                    shared: data.shared,
-                },
-            })
+            let fetchData = {
+                title: data.title,
+                description: data.description,
+                shared: data.shared,
+            }
+            let response = await window.rapidezAPI('PATCH', 'wishlists/' + id, fetchData, { headers: fetchHeaders() })
 
             let wishlist = wishlists.value.find(e => e.id == id)
-            wishlist.title = response.data.title
-            wishlist.description = response.data.description
-            wishlist.shared = response.data.shared
+            wishlist.title = response.title
+            wishlist.description = response.description
+            wishlist.shared = response.shared
         }
 
         return true
@@ -155,21 +115,16 @@ export const update = async function (id, data) {
 
 export const addItem = async function (id, productId) {
     try {
-        let response = await axios({
-            method: 'POST',
-            url: window.url('/api/wishlists/item'),
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-                Store: window.config.store_code,
-            },
-            data: {
-                wishlist_id: id,
-                product_id: productId,
-            },
-        })
+        let fetchData = {
+            wishlist_id: id,
+            product_id: productId,
+        }
+        let response = await window.rapidezAPI('POST', 'wishlists/item', fetchData, { headers: fetchHeaders() })
 
         let wishlist = wishlists.value.find(e => e.id == id)
-        wishlist.items.push(response.data)
+        wishlist.items.push(response)
+
+        return true
     } catch (error) {
         console.log(error)
         Notify(window.config.translations.errors.wrong, 'error')
@@ -180,14 +135,7 @@ export const addItem = async function (id, productId) {
 
 export const removeItem = async function (id, itemId) {
     try {
-        await axios({
-            method: 'DELETE',
-            url: window.url('/api/wishlists/item/' + itemId),
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-                Store: window.config.store_code,
-            },
-        })
+        await window.rapidezAPI('DELETE', 'wishlists/item/' + itemId, {}, { headers: fetchHeaders() })
 
         let wishlist = wishlists.value.find(e => e.id == id)
         let index = wishlist.items.findIndex(e => e.wishlist_item_id == itemId)
@@ -203,23 +151,16 @@ export const removeItem = async function (id, itemId) {
 export const updateItem = async function (id, itemId, data) {
     try {
         if ('description' in data || 'qty' in data) {
-            let response = await axios({
-                method: 'PATCH',
-                url: window.url('/api/wishlists/item/' + itemId),
-                headers: {
-                    Authorization: `Bearer ${token.value}`,
-                    Store: window.config.store_code,
-                },
-                data: {
-                    description: data.description,
-                    qty: data.qty,
-                },
-            })
+            let fetchData = {
+                description: data.description,
+                qty: data.qty,
+            }
+            let response = await window.rapidezAPI('PATCH', 'wishlists/item/' + itemId, fetchData, { headers: fetchHeaders() })
 
             let wishlist = wishlists.value.find(e => e.id == id)
             let item = wishlist.items.find(e => e.wishlist_item_id == itemId)
-            item.description = response.data.description
-            item.qty = response.data.qty
+            item.description = response.description
+            item.qty = response.qty
         }
 
         return true
