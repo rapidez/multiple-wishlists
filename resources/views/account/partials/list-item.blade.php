17dd27d732cb628ec8801c2a0aa17c4bef1f0e29
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2024-04-11 09:46:51 +0200

    Update list-item.blade.php
    Fix when product description is to long for mobile & desktop

--- resources/views/account/partials/list-item.blade.php
+++ resources/views/account/partials/list-item.blade.php
@@ -1,7 +1,7 @@
-<a class="flex items-center gap-x-7 bg-white rounded border pb-5 pt-4 px-7 hover:opacity-75" :href="'{{ route('wishlist.show', '') }}/'+wishlist.id">
-    <x-heroicon-s-heart class="w-5 h-5 text-ct-wishlist" />
+<a class="flex items-start gap-x-7 bg-white rounded border pb-5 pt-4 px-7 hover:opacity-75" :href="'{{ route('wishlist.show', '') }}/'+wishlist.id">
+    <x-heroicon-s-heart class="w-5 h-5 text-ct-wishlist mt-1.5" />
     <div class="flex flex-col">
         <div class="w-full text-base font-medium text-ct-neutral">@lang('Number of articles') (@{{ wishlist.items.length }})</div>
-        <div class="w-full text-sm truncate text-ct-inactive">@{{ wishlist.description }}</div>
+        <div class="w-full text-sm text-ct-inactive max-w-48 max-md:truncate md:max-w-3xl">@{{ wishlist.description }}</div>
     </div>
 </a>
