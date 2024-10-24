b7fcc003f7deacfd373bc817454d6288a83275a6
Jade Geels <a@jgw.tf>
2024-10-24 11:02:20 +0200

    Fix product images in wishlist

--- resources/views/account/partials/details/product/image.blade.php
+++ resources/views/account/partials/details/product/image.blade.php
@@ -1,7 +1,7 @@
 <template v-if="product.thumbnail">
     <td class="w-20 !px-0 max-md:w-16">
         <img
-            v-if="product.image"
+            v-if="product.thumbnail"
             class="object-contain h-16 w-20 shrink-0"
             :alt="product.name"
             :src="`/storage/{{ config('rapidez.store') }}/resizes/200/magento/catalog/product${product.thumbnail}.webp`"
