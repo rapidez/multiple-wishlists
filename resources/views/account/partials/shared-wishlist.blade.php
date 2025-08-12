3e3ddbb03b36d410873d21e2ff4db7771000b614
Jade Geels <a@jgw.tf>
2025-08-12 17:14:51 +0200

    Empty route parameter fix (#46)

--- resources/views/account/partials/shared-wishlist.blade.php
+++ resources/views/account/partials/shared-wishlist.blade.php
@@ -1,3 +1,3 @@
-<a v-if="wishlist.shared" :href="'{{ route('wishlist.shared', '') }}/' + wishlist.sharing_token" class="text-muted text-sm hover:underline">
+<a v-if="wishlist.shared" :href="'{{ route('wishlist.shared', 'replaceme') }}'.replace('replaceme', wishlist.sharing_token)" class="text-muted text-sm hover:underline">
     (@lang('shared'))
 </a>
