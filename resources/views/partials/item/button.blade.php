930a5858ccaf8fb97fad8f56ca05e65b40d05bab
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2025-10-28 10:06:53 +0100

    aria-label on buttons (#52)

--- resources/views/partials/item/button.blade.php
+++ resources/views/partials/item/button.blade.php
@@ -1,8 +1,8 @@
-<button v-if="$root.loggedIn" class="p-3" @click="toggle">
+<button v-if="$root.loggedIn" class="p-3" @click="toggle" aria-label="@lang('Add to wishlist')">
     <x-heroicon-s-heart v-if="isWishlisted({{ $productId }})" class="size-5 text hover:opacity-80" />
     <x-heroicon-o-heart v-else v-bind:class="{ 'text-primary': isOpen, 'text-muted hover:text-primary': !isOpen }" class="size-5 transition" />
 </button>
 
-<button v-else class="p-3" @click="toggle">
+<button v-else class="p-3" @click="toggle" aria-label="@lang('Add to wishlist')">
     <x-heroicon-o-heart v-bind:class="{ 'text-primary': isOpen, 'text-muted hover:text-primary': !isOpen }" class="size-5 transition" />
 </button>
